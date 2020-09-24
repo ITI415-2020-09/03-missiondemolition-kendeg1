78cd253f6747a6105e5c7bd4fe1056494f97995c
kendeg1 <kendeg1@gmail.com>
2020-09-24 15:29:14 -0400

    camera view and other fixes
    1. aprojectiles no longer fly off ground
    2. projectiles bounce and then slow to a stop
    3. camera smoothly pans to projectile that was just fired

---
+++ Assets/Scenes/FollowCam.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCam : MonoBehaviour
+{
+
+    static public FollowCam S; //followCam singleton
+
+    //fields set in the Unity Inspector
+    public float easing = 0.05f;
+    public Vector2 minXY;
+    public bool ________________________________;
+
+    //fields set dynamically
+    public GameObject poi; //point of interest
+    public float camZ; //desired Z pos of the camera
+
+    void Awake()
+    {
+        S = this;
+        camZ = this.transform.position.z;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (poi == null) return; //return if there is no poi
+
+        //get position of the poi
+        Vector3 destination = poi.transform.position;
+        //limit x and y to minimum values
+        destination.x = Mathf.Max(minXY.x, destination.x);
+        destination.y = Mathf.Max(minXY.y, destination.y);
+        //interpolate from the current camera position toward destination
+        destination = Vector3.Lerp(transform.position, destination, easing);
+        //retain a destination.z of camZ
+        destination.z = camZ;
+        //set the camera to the destination
+        transform.position = destination;
+        //set orthographicSize of the camera to keep Ground in view
+        this.GetComponent<Camera>().orthographicSize = destination.y + 10;
+    }
+}
