6b9dec30d987654c24d6bc6e19e6146f13b22749
kendeg1 <kendeg1@gmail.com>
2020-09-24 13:21:58 -0400

    instantiate projectile
    added to script to instantiate a projectile prefab when clicking on launchPoint

--- Assets/Scenes/Slingshot.cs
+++ Assets/Scenes/Slingshot.cs
@@ -3,14 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Slingshot : MonoBehaviour {
-
+    //fields set in unity inspector
+    public GameObject prefabProjectile;
+    public bool _____________________________;
+    
+    //fields set dynamically
     public GameObject launchPoint;
+    public Vector3 launchPos;
+    public GameObject projectile;
+    public bool aimingMode;
 
     void Awake()
     {
         Transform launchPointTrans = transform.Find("LaunchPoint");
         launchPoint = launchPointTrans.gameObject;
         launchPoint.SetActive(false);
+        launchPos = launchPointTrans.position;
     }
 
 
@@ -24,6 +32,18 @@ public class Slingshot : MonoBehaviour {
     {
         //print("Slingshot:OnMouseExit()");
         launchPoint.SetActive(false);
+    }
+
+    void OnMouseDown()
+    {
+        //player has pressed the mouse button while over slingshot
+        aimingMode = true;
+        //instantiate a projectile
+        projectile = Instantiate(prefabProjectile) as GameObject;
+        //start at launchPoint
+        projectile.transform.position = launchPos;
+        //set it to isKinematic for now
+        projectile.GetComponent<Rigidbody>().isKinematic = true;
     }
 
     // Start is called before the first frame update
