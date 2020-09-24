d504eac956ae27601c42676ff3b791e91ba5eef5
kendeg1 <kendeg1@gmail.com>
2020-09-24 13:45:11 -0400

    projectile shoots
    projectile fires when mouse button is released.

--- Assets/Scenes/Slingshot.cs
+++ Assets/Scenes/Slingshot.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Slingshot : MonoBehaviour {
     //fields set in unity inspector
     public GameObject prefabProjectile;
+    public float velocityMult = 4f;
     public bool _____________________________;
     
     //fields set dynamically
@@ -55,6 +56,31 @@ public class Slingshot : MonoBehaviour {
     // Update is called once per frame
     void Update()
     {
-        
+        //if slingshot is not in aimingMode, dont run this code
+        if (!aimingMode) return;
+        //get the current mouse position in 2d screen coords
+        Vector3 mousePos2D = Input.mousePosition;
+        //convert the mouse position to 3d world coordinates
+        mousePos2D.z = -Camera.main.transform.position.z;
+        Vector3 mousePos3D = Camera.main.ScreenToWorldPoint(mousePos2D);
+        //find the change from launchPos to the mousePos3D
+        Vector3 mouseDelta = mousePos3D - launchPos;
+        //limit mouseDelta to the radius of the Slingshot SphereCollider
+        float maxMagnitude = this.GetComponent<SphereCollider>().radius;
+        if (mouseDelta.magnitude > maxMagnitude)
+        {
+            mouseDelta.Normalize();
+            mouseDelta *= maxMagnitude;
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            //the mouse has been released
+            aimingMode = false;
+            projectile.GetComponent<Rigidbody>().isKinematic = false;
+            projectile.GetComponent<Rigidbody>().velocity = -mouseDelta * velocityMult;
+            projectile = null;
+        }
+
     }
 }
