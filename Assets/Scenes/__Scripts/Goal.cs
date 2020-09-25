f51b40b414d54b8f211a095db0a2b2a551568920
kendeg1 <kendeg1@gmail.com>
2020-09-25 11:37:18 -0400

    added castles
    created more castle prefabs

--- Assets/Scenes/__Scripts/Goal.cs
+++ Assets/Scenes/__Scripts/Goal.cs
@@ -1,31 +1,49 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class Goal : MonoBehaviour
-{
+using System.Collections;
+
+
+
+public class Goal : MonoBehaviour {
+
+    // A static field accessible by code anywhere
+
     static public bool goalMet = false;
+
+
 
     void OnTriggerEnter(Collider other)
     {
-        //when the trigger is hit by something
-        //check to see if its a projectile
-        if(other.gameObject.tag == "Projectile")
+
+        // When the trigger is hit by something
+
+        // Check to see if it's a Projectile
+
+        if (other.gameObject.tag == "Projectile")
         {
-            //if so, set goalMet to true
+
+            // If so, set goalMet to true
+
             Goal.goalMet = true;
 
-            //also set the alpha of the color to higher opacity
+            // Also set the alpha of the color to higher opacity
 
             Material mat = GetComponent<Renderer>().material;
+
             Color c = mat.color;
+
             c.a = 1;
+
             mat.color = c;
+
         }
+
     }
 
-    // Start is called before the first frame update
-    void Start()
+
+
+// Start is called before the first frame update
+void Start()
     {
         
     }
